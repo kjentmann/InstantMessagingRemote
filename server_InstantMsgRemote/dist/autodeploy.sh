5207725ec0de571f207f4aa7e16d33691a0c8022
Mads <kjottkak@madPi>
2018-05-06 21:25:18 +0200

    file structure

---
+++ server_InstantMsgRemote/dist/autodeploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp /home/kjottkak/code/InstantMessagingRemote/InstantMessagingRemote_server/dist/InstantMessagingRemote_server.war ~/glassfish4-1-1/glassfish/domains/domain2/autodeploy 
+echo "autodeploy done. Check server status"
