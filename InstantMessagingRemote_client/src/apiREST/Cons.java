542c1e59d3f9cc767b2a340a3851e9014eb4f316
Mads Fornes <mads.fornes@gmail.com>
2018-05-06 21:29:02 +0200

    add server ip adress

--- InstantMessagingRemote_client/src/apiREST/Cons.java
+++ InstantMessagingRemote_client/src/apiREST/Cons.java
@@ -11,7 +11,9 @@ package apiREST;
  */
 public interface Cons {
   
-  String SERVER_REST = "http://localhost:8080/InstantMessagingRemote_server/webresources";
-  String SERVER_WEBSOCKET = "ws://localhost:8080/InstantMessagingRemote_server/ws";
+  //String SERVER_IP="localhost:8080";  //Run server locally
+  String SERVER_IP="46.249.225.122:62987";                //Run server remotly (Norway)<<<<<<<<<<<<<<<<<<<
+  String SERVER_REST = "http://"+SERVER_IP+"/InstantMessagingRemote_server/webresources";
+  String SERVER_WEBSOCKET = "ws://"+SERVER_IP+"/InstantMessagingRemote_server/ws";
   
 }
